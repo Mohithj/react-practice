9a81096469e4373dcff7e1e3986f26c902125a82
Mohith J <70693766+Mohithj@users.noreply.github.com>
2025-07-20 23:46:24 +0530

    react practice

--- sample/starter/src/CounterRedux/Components/counterOne.js
+++ sample/starter/src/CounterRedux/Components/counterOne.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { incrementCount, decrementCount } from "../store/action";
 
 const CounterOne = () => {
-
     const count2 = useSelector((state) => state?.countReducer?.count);
     const dispatch = useDispatch();
     const handleIncrement = () => {
@@ -12,7 +11,6 @@ const CounterOne = () => {
     const handleDecrement = () => {
         dispatch(decrementCount());
     }
-
     return (
         <>
             <button onClick={handleDecrement}>-</button>
